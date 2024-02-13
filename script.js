7f1c914d9e5957dc41ab7f27e46955cb32f91b22
McBood <mcboodvk@gmail.com>
2024-02-13 19:57:26 +0600

    end

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("yesButton").addEventListener("click", function () {
+  window.location.href = "./yes.html"; // замените 'новая_страница.html' на путь к вашему новому HTML файлу
+});
+
+document.getElementById("yesButton1").addEventListener("click", function () {
+  window.location.href = "./yes.html"; // замените 'новая_страница.html' на путь к вашему новому HTML файлу
+});
